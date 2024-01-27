30a63a0c28b85775b6535b8381255fa13db38d76
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-27 09:01:46 +0100

    add: search input

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SeachInput from "../components/SearchInput"
 
 const Header = () => {
   return (
     <div className='flex justify-between px-5 py-[18px] items-center border border-b'>
         <h2 className='text-[20px] font-semibold'>Dashboard</h2>
+        <SeachInput />
         <h2>Justin Bergson</h2>
     </div>
   )
---
+++ components/SearchInput/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div
+      className="relative flex h-12 w-full items-center lg:h-[3.75rem] lg:w-[25rem]"
+    >
+      <img src="" alt="" />
+      <input
+        className=
+          "flex h-10 w-full rounded-md border border-gray-300 bg-white px-[3.5px] py-[2.5] text-sm text-gray-900 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus:ring-2 focus:ring-primaryx-500 focus:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50  dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+      />
+    </div>
+  )
+}
+
+export default index
