ba953a4bea9efec398d32ded4a0b11d4032bcf53
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-27 07:11:41 +0100

    done w sidde

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,8 +8,8 @@ import { FiSettings } from 'react-icons/fi';
 const Sidebar = ({ children }) => {
   return (
     <div className='flex'>
-      <div className='fixed w-20 h-screen p-4 bg-[#F7F8FA] border-r-[1px] flex flex-col justify-between'>
-        <div className='flex flex-col items-center justify-between'>
+      <div className='absolute w-20 h-full p-4 bg-[#F7F8FA] border-r-[1px] flex flex-col justify-between'>
+        <div className='flex flex-col items-center'>
           <Link href='/'>
             <div className=' text-white p-3 rounded-lg inline-block'>
               <img width={40} src="/logo.png" height={40} alt='logo'/>
@@ -17,32 +17,32 @@ const Sidebar = ({ children }) => {
           </Link>
           {/* <span className='border-b-[1px] border-gray-200 w-full p-2'></span> */}
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
               <Image width={40} src="/icon1.png" height={40} alt='icon1'/>
             </div>
           </Link>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
               <Image width={40} src="/trend-up.png" height={40} alt='trendup'/>
             </div>
           </Link>
           <Link href='/customers'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
               <Image width={40} src="/profile-2user.svg" height={40} alt='profile'/>    
             </div>
           </Link>
           <Link href='/orders'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
               <Image width={40} src="/box.svg" height={40} alt='orders'/>
             </div>
           </Link>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <Image width={40} src="/setting-2.svg" height={40} alt='setting'/>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
+              <Image width={40} src="/discount-shape.svg" height={40} alt='setting'/>
             </div>
           </Link>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block'>
               <Image width={40} src="/info-circle.svg" height={40} alt='info'/>
             </div>
           </Link>
@@ -57,6 +57,23 @@ const Sidebar = ({ children }) => {
             <Link href='/'>
               <div className=' hover:bg-gray-200 cursor-pointer my-1 p-1 rounded-lg inline-block'>
                 <Image width={40} src="/moon 1.svg" height={40} alt='info'/>
+              </div>
+            </Link>
+          </div>
+
+          <div className="mt-28">
+            <Link href='/'>
+              <div className=' hover:bg-gray-200 cursor-pointer my-1 p-3 rounded-full inline-block'>
+                <Image width={40} src="/arrow-right.svg" height={40} alt='info'/>
+              </div>
+            </Link>
+            <Link href='/'>
+              <div className=' hover:bg-gray-200 cursor-pointer my-1 p-3 rounded-full inline-block'>
+                <Image width={40} src="/setting-2.svg" height={40} alt='info'/>
+              </div>
+            </Link><Link href='/'>
+              <div className=' hover:bg-gray-200 cursor-pointer my-1 p-3 rounded-full inline-block'>
+                <Image width={40} src="/logout.svg" height={40} alt='info'/>
               </div>
             </Link>
           </div>
