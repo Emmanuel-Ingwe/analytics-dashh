eda369fb5aff4a39efa0116803bb350a2ad8b8c0
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-27 06:20:10 +0100

    add: iconss

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,32 +8,42 @@ import { FiSettings } from 'react-icons/fi';
 const Sidebar = ({ children }) => {
   return (
     <div className='flex'>
-      <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
+      <div className='fixed w-20 h-screen p-4 bg-[#F7F8FA] border-r-[1px] flex flex-col justify-between'>
         <div className='flex flex-col items-center'>
           <Link href='/'>
             <div className=' text-white p-3 rounded-lg inline-block'>
-              <Image width={20} src="/logo.png" height={20} alt='logo'/>
+              <Image width={40} src="/logo.png" height={40} alt='logo'/>
             </div>
           </Link>
-          <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
+          {/* <span className='border-b-[1px] border-gray-200 w-full p-2'></span> */}
           <Link href='/'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <RxDashboard size={20} />
+              <Image width={40} src="/icon1.png" height={40} alt='icon1'/>
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <Image width={40} src="/trend-up.png" height={40} alt='trendup'/>
             </div>
           </Link>
           <Link href='/customers'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <RxPerson size={20} />
+              <Image width={40} src="/profile-2user.svg" height={40} alt='profile'/>    
             </div>
           </Link>
           <Link href='/orders'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <HiOutlineShoppingBag size={20} />
+              <Image width={40} src="/box.svg" height={40} alt='orders'/>
             </div>
           </Link>
           <Link href='/'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <FiSettings size={20} />
+              <Image width={40} src="/setting-2.svg" height={40} alt='setting'/>
+            </div>
+          </Link>
+          <Link href='/'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+              <Image width={40} src="/info-circle.svg" height={40} alt='info'/>
             </div>
           </Link>
         </div>
