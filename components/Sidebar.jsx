a057cca438030644ad7438d9740635f25b1c51e5
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-27 06:00:13 +0100

    add: logo

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,8 +11,8 @@ const Sidebar = ({ children }) => {
       <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
         <div className='flex flex-col items-center'>
           <Link href='/'>
-            <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
-              <RxSketchLogo size={20} />
+            <div className=' text-white p-3 rounded-lg inline-block'>
+              <Image width={20} src="/logo.png" height={20} alt='logo'/>
             </div>
           </Link>
           <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"/>
       </Head>
+      
       <main className='bg-gray-100 min-h-screen'>
         <Header />
         <TopCards />
