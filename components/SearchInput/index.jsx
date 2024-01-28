0726b16bade4fa8bc326b2f5636881c17ff2069f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-28 23:16:54 +0100

    ..

--- components/SearchInput/index.jsx
+++ components/SearchInput/index.jsx
@@ -8,7 +8,7 @@ const index = () => {
       <img src="iconsearch.png" alt="" className='absolute left-3' />
       <input placeholder='Search...'
         className=
-          "flex h-12 w-full rounded-[24px] border-[#DADDDD] pl-[36px] bg-white px-[3.5px] py-[2.5] text-sm text-gray-900  file:text-sm file:font-medium placeholder:text-slate-500 focus:ring-2 focus:ring-primaryx-500 focus:ring-offset-2 disabled:opacity-50"
+          "flex h-12 w-full rounded-[24px] border outline-none pl-[36px] bg-white px-[3.5px] py-[2.5] text-sm text-gray-900  file:text-sm file:font-medium placeholder:text-slate-500"
       />
     </div>
   )
