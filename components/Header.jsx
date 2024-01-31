3fb03cd8e6478cb17db9ccd8bc662c890ae4f52c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-31 23:58:42 +0100

    add: notify-icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,8 +7,12 @@ const Header = () => {
         <h2 className='text-[20px] font-semibold'>Dashboard</h2>
         <div className="flex">
           <SeachInput />
-          <div className="flex">
-            <img src="/" alt="" />
+          <div className="flex items-center justify-center">
+            <img src="/solar-calender.png" alt="calender-icon" />
+            <h1 className="">1 February, 2024</h1>
+          </div>
+          <div className="rounded-full items-center">
+            <img src="/notification-icon.png" alt="" />
           </div>
           <div className="flex border rounded-full p-4">
           <h2>Justin Bergson</h2>
