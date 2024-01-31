c07ee01d5179204bc61448f8d090e6237b427438
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-01 00:03:13 +0100

    ..

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,14 @@ const Header = () => {
   return (
     <div className='flex justify-between px-5 py-[18px] items-center border border-b'>
         <h2 className='text-[20px] font-semibold'>Dashboard</h2>
-        <div className="flex">
+
+        <div className="flex items-center justify-between">
           <SeachInput />
           <div className="flex items-center justify-center">
             <img src="/solar-calender.png" alt="calender-icon" />
-            <h1 className="">1 February, 2024</h1>
+            <h1 className="">February 1, 2024</h1>
           </div>
-          <div className="rounded-full items-center">
+          <div className="flex border p-2 rounded-full items-center">
             <img src="/notification-icon.png" alt="" />
           </div>
           <div className="flex border rounded-full p-4">
