109b7f92fe1816e12cf40343f802688c9dff8b59
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-03 20:39:38 +0100

    add:__

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -16,7 +16,7 @@ const TopCards = () => {
             <div className='flex flex-col w-full'>
                 <p className='text-xl font-medium text-[#898989] py-2'>{item.status}</p>
                 <p className='font-semibold text-2xl'>{item.digits}</p>
-                <div className="flex pt-3 justify-between">
+                <div className="flex pt-3 justify-between cursor-pointer">
                     {item.rate}
                     <p className="text-gray-500">{item.month}</p>
                 </div> 
