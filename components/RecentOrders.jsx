8a6b791f41f035b925327ece04d82418b6666e0f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-05 10:26:26 +0100

    ..

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -7,7 +7,7 @@ const RecentOrders = () => {
     <div className='w-full col-span-1 relative h-[40vh] m-auto p-4 rounded-lg bg-white overflow-y-scroll'>
       <div className="flex items-center justify-between">
         <h1 className='font-semibold text-lg'>Top Platform</h1>
-        <h2 className="text-lg font-medium text-[#34caa5]">See All</h2>
+        <h2 className="text-lg font-medium text-[#34caa 5]">See All</h2>
       </div>
       <ul>
         {data.map((order, id) => (
