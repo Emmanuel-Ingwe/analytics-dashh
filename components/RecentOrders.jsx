8d875ff2023653107e58a73070c2cfa4ab1d0363
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-05 10:07:20 +0100

    //

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -13,9 +13,8 @@ const RecentOrders = () => {
         {data.map((order, id) => (
           <div className="py-4">
             <h1>{order.name}</h1>
-            <div className="relative flex items-center w-full h-3 rounded-[40px] bg-[#f5f5f5]">
-              <div className="">{order.style}</div>
-              <input type="range" className='w-full'/>
+            <div className="flex items-center w-full h-3 rounded-[40px] bg-[#f5f5f5]">
+              {/* <input type="range" className='w-full bg-[#f5f5f5] border-none'/> */}
             </div>
             <div className="flex items-center justify-between">
               <h1 className="">{order.amount}</h1>
