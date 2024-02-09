b67a7187d211e6c4277b071e52932328836750dd
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-09 22:34:30 +0100

    classname-prop

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -10,20 +10,7 @@ const RecentOrders = () => {
         <h2 className="text-lg font-medium text-[#34caa5]">See All</h2>
       </div>
       <ul>
-        {data.map((order, id) => (
-          <div className="py-4">
-            <h1 className='font-semibold text-lg'>{order.name}</h1>
-            <div className="flex items-center w-full before h-3 rounded-[40px] bg-[#f5f5f5] my-4">
-              {/* <input type="range" className='w-full bg-[#f5f5f5] border-none'/> */}
-            </div>
-            <div className="flex items-center justify-between text-[#22242C]">
-              <h1 className="font-normal text-lg">{order.amount}</h1>
-              <h1 className="font-normal text-lg">{order.percentage}</h1>
-            </div>
-          </div>
-
-          /*Delete AND MAKE A COMPONENT OF THAT ABOVE */
-        ))}
+       
       </ul>
     </div>
   );
