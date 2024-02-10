7e7e1d2eb51902d7d4eefa9cd2b4bf1a22e7c9ab
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-10 20:28:30 +0100

    dd:Range

--- components/Range/index.jsx
+++ components/Range/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
 
-const index = (className) => {
+const index = (props) => {
   return (
-    <div className="py-4">
-            <h1 className='font-semibold text-lg'>{order.name}</h1>
-            <div className="flex items-center w-full before h-3 rounded-[40px] bg-[#f5f5f5] my-4">
-                <div className={className}></div>
-            </div>
-            <div className="flex items-center justify-between text-[#22242C]">
-              <h1 className="font-normal text-lg">{order.amount}</h1>
-              <h1 className="font-normal text-lg">{order.percentage}</h1>
-            </div>
-          </div>
+    <div className="">s</div>
   )
 }
 
--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { data } from '../data/data.js';
 import { FaShoppingBag } from 'react-icons/fa';
+import Range from "../components/Range"
 
 const RecentOrders = () => {
   return (
@@ -10,7 +11,16 @@ const RecentOrders = () => {
         <h2 className="text-lg font-medium text-[#34caa5]">See All</h2>
       </div>
       <ul>
-       
+        <div className={`py-4 ${props.className}`}>
+              <h1 className='font-semibold text-lg'>{order.name}</h1>
+              <div className="flex items-center w-full before h-3 rounded-[40px] bg-[#f5f5f5] my-4">
+                  <Range className="w" />
+              </div>
+              <div className="flex items-center justify-between text-[#22242C]">
+                <h1 className="font-normal text-lg">{order.amount}</h1>
+                <h1 className="font-normal text-lg">{order.percentage}</h1>
+              </div>
+        </div>
       </ul>
     </div>
   );
