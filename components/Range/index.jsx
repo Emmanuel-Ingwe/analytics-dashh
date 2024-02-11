ff6bc137a3cdbec1841ee560ce4f6ef7328ee963
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-02-11 16:46:19 +0100

    //

--- components/Range/index.jsx
+++ components/Range/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index = ({bgColor, width}) => {
   return (
-    <div className={`dd ${bgColor} ${width} h-3 absolute rounded-full `}>s</div>
+    <div className={`dd ${bgColor} ${width} h-3 absolute rounded-full `}></div>
   )
 }
 
