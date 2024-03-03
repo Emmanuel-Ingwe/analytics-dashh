30d5c5ed99d8445b8c65934be02cacb7cd5b8da9
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-03-03 23:44:38 +0100

    add: country

--- components/LastOrders/index.jsx
+++ components/LastOrders/index.jsx
@@ -16,7 +16,7 @@ const index = () => {
             <th className="px-4 py-2">Date</th>
             <th className="px-4 py-2">Amount</th>
             <th className="px-4 py-2">Status</th>
-            <th className="px-4 py-2">Price</th>
+            <th className="px-4 py-2">country</th>
             </tr>
         </thead>
         <tbody>
